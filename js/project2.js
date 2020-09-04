6ba912bfe97feee1742435db8fbd84626be03800
yasmine-sghir <69588871+yasmine-sghir@users.noreply.github.com>
2020-09-04 12:58:54 +0100

    Add files via upload

---
+++ js/project2.js
@@ -0,0 +1,266 @@
+var zodiac = [
+    {
+        sign: "ARIES",
+        text: "March 21 - April 19 <br> Monthly horoscope: Welcome to the most pivotal month of your year. September brings that back-to-school feeling, but under the lingering pandemic vibes, it’s far different than it’s ever been before. A deeply emotional Full Moon in Pisces washes you clean on the early morning of the 2nd. This lunation parts the waters in your dream zone, making you into a psychic vessel. This can be overwhelming, so practice lots of self-care. You’re letting go of a lot of the trauma of the last six months, and it may feel uncomfortable, but deeply healing – and after a few days, you should be flooded with relief. ",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/aries-julie-weatherley.jpg"
+    },
+    {
+        sign: "TAURUS",
+        text: "April 20 - May 20 <br> September is brimming with deep, earthy vibes, Taurus – and this is so grounding for you. The Full Moon in Pisces opens the floodgates in your social zone early on the 2nd, allowing you to release any sadness, pent-up frustration, or just bad feelings you’ve held onto during the last six months – since the pandemic began.",
+        image: "https://images.fineartamerica.com/images/artworkimages/medium/1/taurus-julie-weatherley.jpg"
+    },
+    {
+        sign: "GEMINI",
+        text: "May 21 - June 20 <br> Summer is fading away, Gemini, and this month brings you back to your roots. Family and close friends that feel like family are your EVERTHING right now. The Full Moon in Pisces brings all the feels early on the 2nd, as this lunation helps you release all your fear and trauma of the last six months. If your career ambitions were put on pause or just crashed because of the pandemic, let it all out. You might find yourself at a creative crossroads, so tune into your dreams – there’s plenty of material to mine.",
+        image: "https://i.pinimg.com/474x/08/7b/c4/087bc4ca138e4199f13be1ad9f85bfd6.jpg"
+    },
+    {
+        sign: "CANCER",
+        text: "June 21 - July 22 <br> The last month of summer is upon us, but the fall is about to bring you a first or two, Cancer. The Full Moon in Pisces, your sister water sign, cleanses your soul with a tsunami of power early on the 2nd. This plunges you into a magical portal of healing, helping you to recalibrate after six months of fear and trauma from the pandemic. You’re seeing the world in an entirely new way now. Heed the intensity of Mars Retrograde, beginning on 9 September and lasting through mid-November. With the planet of passion stalled in your career zone, major frustrations can arise in your professional life. The key is not to let them consume you with hot rage, but to also express your anger when you do feel it. Whatever you do – don’t turn it in on yourself.",
+        image: "https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/1/cancer-the-crab-julie-weatherley.jpg"
+    },
+    {
+        sign: "LEO",
+        text: "July 23 - August 22 <br> Keep your mind on your money this September, because the stars can help you tap that cash and make a fast stash. The Full Moon in Pisces releases a floodgate of feelings in your intimacy zone early on the 2nd. This gives voice to emotions you’ve carried for the last six months, since the pandemic began. If you’ve felt deeply emotionally vulnerable but held it inside for fear of expressing something too raw, you may not be able to control it anymore – and that’s a good thing. Let it out – all of it.",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/leo-julie-weatherley.jpg"
+    },
+    {
+        sign: "VIRGO",
+        text: "August 23 - September 22 <br> It’s your birthday season! It’s still a bit weird to celebrate your solar return as a pandemic roars through the world, but you simply must find ways to honor your birthright all month long. The Full Moon in Pisces opens the month on a powerful note early on the 2nd, so expect an ocean of emotion at the start of September. This opens the floodgates in your relationship zone, forcing you to deal with any issues that have come up over the last six months – especially any material you avoided to keep yourself safe.",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/virgo-julie-weatherley.jpg"
+    },
+    {
+        sign: "LIBRA",
+        text: "September 23 - October 22 <br> The last month of summer is upon us, Libra , and your birthday season is right around the corner. With the Sun in your house of sleep and dreams, make yourself into an expert napper and meditator this month. It’s ideal to get as much beauty sleep as possible before you start hardcore celebrating in a few weeks, and don’t skimp on the inner peace. A sleepy, mystical Full Moon in Pisces lands in your work zone early on the 2nd, helping you to release six months of pent-up stress about your everyday life. ",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/libra-julie-weatherley.jpg"
+    },
+    {
+        sign: "SCORPIO",
+        text: "October 23 - November 2 <br> Your social life is absolutely gorgeous this month, and you’re more smitten by your friends than you’ve been in a long time. But before you sign up for another Zoom cocktail party, immerse yourself in the mysterious magic of the Full Moon in Pisces on the 2nd. This powerful lunation in your sister water sign acts as a release valve for your last six months – especially for romance, creativity and pleasure. If you sought any of those blissful experiences but the pandemic got in the way, it’s ok to feel sad – and right now is the perfect moment to let it all out so you can heal.",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/scorpio-julie-weatherley.jpg"
+    },
+    {
+        sign: "SAGITTARIUS",
+        text: "November 22 - December 21 <br> As late summer gives way to early fall, your career is on absolute fire, Sagittarius. There’s so much awesome professional prowess to tap it’s hard to know where to start. Before you begin to think about your future, take a moment to reflect on the last six months. That’s precisely what the Full Moon in Pisces asks of you early on the 2nd, when it unleashes a spigot of emotion in your home zone. So much has happened since the pandemic began last March, and releasing any remaining pain, trauma or hardship is extremely healing at this moment – even if it scares you.",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/sagittarius-julie-weatherley.jpg"
+    },
+    {
+        sign: "CAPRICORN",
+        text: "December 22 - January 19 <br> The stars are practically begging you to take a serious staycation this month, Capricorn – especially if you’ve been working too hard to give yourself this gift in the last few months. So sit back, grab a novel and a cocktail, and fantasize about the trips you’ll go on next year. The Full Moon in Pisces opens the month on the dreamiest note possible, with a mystical invitation into the mysteries of your own mind. This is also ideal for releasing any trauma you’ve held onto since the pandemic began last March. We’re at the six-month mark; check in with yourself to make sure you’re truly ok and not putting on a strong face for other people – put your own oxygen mask on first at this lunation. Note that writing in your journal is particularly potent and healing at the moment.",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/capricorn-julie-weatherley.jpg"
+    },
+    {
+        sign: "AQUARIUS",
+        text: "January 20 - February 18 <br> The stars are revving up your desire during the last few weeks of summer. With the Sun in your house of transformation, everything in your life feels extra resonant. The Full Moon in Pisces lands early on the 2nd, emphasizing this theme and amplifying your search for self-worth. The last six months of hardship and trauma are ripe for release, and since you tend not to let yourself examine your own emotions, it could seem like a tsunami of feelings are coming out all at once. But that’s good, Aquarius – don’t be afraid of it.",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/aquarius-julie-weatherley.jpg"
+    },
+    {
+        sign: "PISCES",
+        text: "February 19 - March 20 <br> September is a supremely special month for you, Pisces, highlighting your most significant relationships and helping you navigate potent partnership issues. This starts right at the beginning with the magical, annual Full Moon in your very own stars early on the 2nd. This is one of the most significant moments of your entire year and it’s deeply emotional. It’s as if a portal is opened in the universe and all the feelings you’ve felt during the last six months of the pandemic are flooding through it all at once. Let go, let go, and let go some more.",
+        image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/pisces-julie-weatherley.jpg"
+    }
+    ]
+    
+    function horoscopeMe(){
+        var horoscope = document.getElementById("horoscope").value.toUpperCase()
+    
+        for (i = 0; i < zodiac.length; i++) {
+            if (horoscope === zodiac[i].sign) {
+                document.getElementById("symbols").src = zodiac[i].image
+                document.getElementById("yourSign").innerHTML = zodiac[i].sign
+                document.getElementById("fortune").innerHTML = zodiac[i].text
+                
+                return
+            }   else {
+                document.getElementById("yourSign").innerHTML = "Unknown"
+                document.getElementById("fortune").innerHTML = "I don't know about that one buddy!" 
+                
+            }
+        }
+    }
+    
+    // For the Background
+    
+    
+    (function() {
+    
+        var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+    
+        // Main
+        initHeader();
+        initAnimation();
+        addListeners();
+    
+        function initHeader() {
+            width = window.innerWidth;
+            height = window.innerHeight;
+            target = {x: width/2, y: height/2};
+    
+            
+            canvas = document.getElementById('demo-canvas');
+            canvas.width = width;
+            canvas.height = height;
+            ctx = canvas.getContext('2d');
+    
+            // create points
+            points = [];
+            for(var x = 0; x < width; x = x + width/20) {
+                for(var y = 0; y < height; y = y + height/20) {
+                    var px = x + Math.random()*width/20;
+                    var py = y + Math.random()*height/20;
+                    var p = {x: px, originX: px, y: py, originY: py };
+                    points.push(p);
+                }
+            }
+    
+            // for each point find the 5 closest points
+            for(var i = 0; i < points.length; i++) {
+                var closest = [];
+                var p1 = points[i];
+                for(var j = 0; j < points.length; j++) {
+                    var p2 = points[j]
+                    if(!(p1 == p2)) {
+                        var placed = false;
+                        for(var k = 0; k < 5; k++) {
+                            if(!placed) {
+                                if(closest[k] == undefined) {
+                                    closest[k] = p2;
+                                    placed = true;
+                                }
+                            }
+                        }
+    
+                        for(var k = 0; k < 5; k++) {
+                            if(!placed) {
+                                if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                    closest[k] = p2;
+                                    placed = true;
+                                }
+                            }
+                        }
+                    }
+                }
+                p1.closest = closest;
+            }
+    
+            // assign a circle to each point
+            for(var i in points) {
+                var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+                points[i].circle = c;
+            }
+        }
+    
+        // Event handling
+        function addListeners() {
+            if(!('ontouchstart' in window)) {
+                window.addEventListener('mousemove', mouseMove);
+            }
+            window.addEventListener('scroll', scrollCheck);
+            window.addEventListener('resize', resize);
+        }
+    
+        function mouseMove(e) {
+            var posx = posy = 0;
+            if (e.pageX || e.pageY) {
+                posx = e.pageX;
+                posy = e.pageY;
+            }
+            else if (e.clientX || e.clientY)    {
+                posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+            }
+            target.x = posx;
+            target.y = posy;
+        }
+    
+        function scrollCheck() {
+            if(document.body.scrollTop > height) animateHeader = false;
+            else animateHeader = true;
+        }
+    
+        function resize() {
+            width = window.innerWidth;
+            height = window.innerHeight;
+            canvas.width = width;
+            canvas.height = height;
+        }
+    
+        // animation
+        function initAnimation() {
+            animate();
+            for(var i in points) {
+                shiftPoint(points[i]);
+            }
+        }
+    
+        function animate() {
+            if(animateHeader) {
+                ctx.clearRect(0,0,width,height);
+                for(var i in points) {
+                    // detect points in range
+                    if(Math.abs(getDistance(target, points[i])) < 4000) {
+                        points[i].active = 0.3;
+                        points[i].circle.active = 0.6;
+                    } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                        points[i].active = 0.1;
+                        points[i].circle.active = 0.3;
+                    } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                        points[i].active = 0.02;
+                        points[i].circle.active = 0.1;
+                    } else {
+                        points[i].active = 0;
+                        points[i].circle.active = 0;
+                    }
+    
+                    drawLines(points[i]);
+                    points[i].circle.draw();
+                }
+            }
+            requestAnimationFrame(animate);
+        }
+    
+        function shiftPoint(p) {
+            TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
+                y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
+                onComplete: function() {
+                    shiftPoint(p);
+                }});
+        }
+    
+        // Canvas manipulation
+        function drawLines(p) {
+            if(!p.active) return;
+            for(var i in p.closest) {
+                ctx.beginPath();
+                ctx.moveTo(p.x, p.y);
+                ctx.lineTo(p.closest[i].x, p.closest[i].y);
+                ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+                ctx.stroke();
+            }
+        }
+    
+        function Circle(pos,rad,color) {
+            var _this = this;
+    
+            // constructor
+            (function() {
+                _this.pos = pos || null;
+                _this.radius = rad || null;
+                _this.color = color || null;
+            })();
+    
+            this.draw = function() {
+                if(!_this.active) return;
+                ctx.beginPath();
+                ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+                ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+                ctx.fill();
+            };
+        }
+    
+        // Util
+        function getDistance(p1, p2) {
+            return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+        }
+        
+    })();
